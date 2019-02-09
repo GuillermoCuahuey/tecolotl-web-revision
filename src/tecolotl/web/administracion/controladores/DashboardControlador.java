f94e93af1f51bc1fb6805a8a544afdd32df0c6fb
GuillermoCuahuey <cu.es.guillermo@gmail.com>
2019-02-09 16:06:25 -0600

    dashboard navegacion

--- src/tecolotl/web/administracion/controladores/DashboardControlador.java
+++ src/tecolotl/web/administracion/controladores/DashboardControlador.java
@@ -1,43 +1,27 @@
 package tecolotl.web.administracion.controladores;
 
 import tecolotl.web.administracion.modelo.EscuelaModelo;
+import tecolotl.web.controlador.PaginacionControlador;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Model;
-import javax.faces.component.html.HtmlDataTable;
-import javax.inject.Named;
+ import javax.inject.Named;
 import java.util.ArrayList;
 import java.util.List;
 
 @RequestScoped
 @Named
-public class DashboardControlador {
+public class DashboardControlador extends PaginacionControlador<EscuelaModelo> {
 
     private List<EscuelaModelo> escuelaModeloLista;
-    private HtmlDataTable htmlDataTable;
 
     @PostConstruct
     public void init() {
         escuelaModeloLista = new ArrayList<>();
         for (int i=0; i < 30; i++) {
-            escuelaModeloLista.add(new EscuelaModelo("wqweqweqe", "Benito Juarex", true, 3, 21));
+            escuelaModeloLista.add(new EscuelaModelo("cct" + i, "Nombre Escuela", i % 2 == 0, 3, 21));
         }
+        getCollectionDataModel().setWrappedData(escuelaModeloLista);
     }
 
-    public List<EscuelaModelo> getEscuelaModeloLista() {
-        return escuelaModeloLista;
-    }
-
-    public void setEscuelaModeloLista(List<EscuelaModelo> escuelaModeloLista) {
-        this.escuelaModeloLista = escuelaModeloLista;
-    }
-
-    public HtmlDataTable getHtmlDataTable() {
-        return htmlDataTable;
-    }
-
-    public void setHtmlDataTable(HtmlDataTable htmlDataTable) {
-        this.htmlDataTable = htmlDataTable;
-    }
 }
