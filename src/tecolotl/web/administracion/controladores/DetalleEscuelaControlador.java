14c517690345a46b4d5a548b88ebaea309acb51b
Maugg <maugg76@gmail.com>
2019-03-02 19:29:15 -0600

    modulos controladores y vista detalle profesor del coordinador

--- src/tecolotl/web/administracion/controladores/DetalleEscuelaControlador.java
+++ src/tecolotl/web/administracion/controladores/DetalleEscuelaControlador.java
@@ -1,9 +1,6 @@
 package tecolotl.web.administracion.controladores;
 
-import tecolotl.web.administracion.modelo.ContactoModelo;
-import tecolotl.web.administracion.modelo.EscuelaDetalleModelo;
-import tecolotl.web.administracion.modelo.EscuelaModelo;
-import tecolotl.web.administracion.modelo.LicenciasModelo;
+import tecolotl.web.administracion.modelo.*;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -17,17 +14,31 @@ public class DetalleEscuelaControlador  {
     private EscuelaDetalleModelo escuelaDetalleModelos;
     private List<ContactoModelo> contactoModelo;
     private List<LicenciasModelo> licenciasModelo;
+    private List<TipoContactoModelo> tipoContactoModelos;
     @PostConstruct
     public void init() {
             escuelaDetalleModelos = new EscuelaDetalleModelo("21DBA0004C", "Escuela de algun lugar",3,"72758","Puebla","Puebla","calle siempre viva",54,0,"activo","margarita");
             contactoModelo = new ArrayList<>();
             licenciasModelo = new ArrayList<>();
+            tipoContactoModelos = new ArrayList<>();
         for (int i=0; i <= 4; i++) {
             contactoModelo.add(new ContactoModelo("Juan Pancracio Filomeno","Coordinador","2226967841"));
         }
         for (int i=0; i<=2;i++) {
             licenciasModelo.add(new LicenciasModelo(i+1,"21DBA0004C",i*i-i));
         }
+        for (int i=0;i<3;i++){
+            tipoContactoModelos.add(new TipoContactoModelo("TipoContacto".concat(String.valueOf(i))));
+        }
+
+    }
+
+    public List<TipoContactoModelo> getTipoContactoModelos() {
+        return tipoContactoModelos;
+    }
+
+    public void setTipoContactoModelos(List<TipoContactoModelo> tipoContactoModelos) {
+        this.tipoContactoModelos = tipoContactoModelos;
     }
 
     public List<LicenciasModelo> getLicenciasModelo() {
---
+++ src/tecolotl/web/administracion/modelo/TipoContactoModelo.java
@@ -0,0 +1,17 @@
+package tecolotl.web.administracion.modelo;
+
+public class TipoContactoModelo {
+    private String tipoContaco;
+
+    public TipoContactoModelo(String tipoContaco) {
+        this.tipoContaco = tipoContaco;
+    }
+
+    public String getTipoContaco() {
+        return tipoContaco;
+    }
+
+    public void setTipoContaco(String tipoContaco) {
+        this.tipoContaco = tipoContaco;
+    }
+}
