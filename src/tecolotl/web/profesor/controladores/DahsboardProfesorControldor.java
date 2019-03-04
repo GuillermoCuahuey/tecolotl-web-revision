3c9ee3fb1e0d6f9602404e8ca0409da62dd25d4f
Maugg <maugg76@gmail.com>
2019-03-03 22:57:39 -0600

    dashboard profesor img

--- src/tecolotl/web/profesor/controladores/DahsboardProfesorControldor.java
+++ src/tecolotl/web/profesor/controladores/DahsboardProfesorControldor.java
@@ -1,7 +1,6 @@
 package tecolotl.web.profesor.controladores;
 
-import tecolotl.web.profesor.modulo.ProfesorModelo;
-import tecolotl.web.profesor.modulo.SalonModelo;
+import tecolotl.web.profesor.modulo.*;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
@@ -14,13 +13,17 @@ import java.util.List;
 public class DahsboardProfesorControldor {
     private List<ProfesorModelo> profesorModelos;
     private List<SalonModelo> salonModelos;
-    private List<String> color;
-
+    private List<ColorModelo> colorModelos;
+    private List<GraficaModelo> graficaModelos;
+    private List<DatosModelo> datosModelos;
     @PostConstruct
     public void init(){
         profesorModelos = new ArrayList<>();
         salonModelos = new ArrayList<>();
-        color = new ArrayList<>();
+        colorModelos = new ArrayList<>();
+        graficaModelos = new ArrayList<>();
+        datosModelos = new ArrayList<>();
+
         for (int i = 0; i<4 ; i++){
             profesorModelos.add(new ProfesorModelo("Profesor".concat(String.valueOf(i)),"Apellido".concat(String.valueOf(i)),10,""));
         }
@@ -28,10 +31,19 @@ public class DahsboardProfesorControldor {
             salonModelos.add(new SalonModelo(i+1, Character.getName(97 + i)));
 
         }
-        color.add("rgba(51,153,255,0.6)");
-        color.add("rgba(60, 186, 159,0.6)");
-        color.add("rgba(137, 93, 246,0.6)");
-        color.add("rgba(232, 195, 185,0.6)");
+
+        graficaModelos.add(new GraficaModelo("A",1,"rgba(51,153,255,0.6)",8.5f,10.0f,9.3f,8.6f,7.8f));
+        graficaModelos.add(new GraficaModelo("B",1,"rgba(36,113,163,0.6)",7.5f,9.0f,8.7f,7.7f,10.0f));
+        graficaModelos.add(new GraficaModelo("C",1,"rgba(20,143,119,0.6)",9.5f,8.0f,2.3f,8.6f,9.8f));
+        graficaModelos.add(new GraficaModelo("D",1,"rgba(183,149,11,0.6)",10.0f,9.0f,8.3f,9.6f,8.8f));
+
+        colorModelos.add(new ColorModelo("rgba(51,153,255,0.6)"));
+        colorModelos.add(new ColorModelo("rgba(36,113,163,0.6)"));
+        colorModelos.add(new ColorModelo("rgba(20,143,119,0.6)"));
+        colorModelos.add(new ColorModelo("rgba(183,149,11,0.6)"));
+
+
+
 
     }
 
@@ -52,11 +64,22 @@ public class DahsboardProfesorControldor {
         this.salonModelos = salonModelos;
     }
 
-    public List<String> getColor() {
-        return color;
+
+    public List<ColorModelo> getColorModelos() {
+        return colorModelos;
     }
 
-    public void setColor(List<String> color) {
-        this.color = color;
+    public void setColorModelos(List<ColorModelo> colorModelos) {
+        this.colorModelos = colorModelos;
     }
+
+    public List<GraficaModelo> getGraficaModelos() {
+        return graficaModelos;
+    }
+
+    public void setGraficaModelos(List<GraficaModelo> graficaModelos) {
+        this.graficaModelos = graficaModelos;
+    }
+
+
 }
---
+++ src/tecolotl/web/profesor/modulo/DatosModelo.java
@@ -0,0 +1,59 @@
+package tecolotl.web.profesor.modulo;
+
+public class DatosModelo {
+    private float dato1;
+    private float dato2;
+    private float dato3;
+    private float dato4;
+    private float dato5;
+
+    public DatosModelo(float dato1, float dato2, float dato3, float dato4, float dato5) {
+        this.dato1 = dato1;
+        this.dato2 = dato2;
+        this.dato3 = dato3;
+        this.dato4 = dato4;
+        this.dato5 = dato5;
+    }
+
+    public float getDato1() {
+        return dato1;
+    }
+
+    public void setDato1(float dato1) {
+        this.dato1 = dato1;
+    }
+
+    public float getDato2() {
+        return dato2;
+    }
+
+    public void setDato2(float dato2) {
+        this.dato2 = dato2;
+    }
+
+    public float getDato3() {
+        return dato3;
+    }
+
+    public void setDato3(float dato3) {
+        this.dato3 = dato3;
+    }
+
+    public float getDato4() {
+        return dato4;
+    }
+
+    public void setDato4(float dato4) {
+        this.dato4 = dato4;
+    }
+
+    public float getDato5() {
+        return dato5;
+    }
+
+    public void setDato5(float dato5) {
+        this.dato5 = dato5;
+    }
+}
+
+
